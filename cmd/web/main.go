a4b2c8f89c9984f03d2fcb06ceed1c1977ae8326
David Kim <dkhw90@gmail.com>
2024-10-21 23:35:16 +0200

    Add basic handlers, router, & views

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,28 +1,42 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+// inject dependencies by using application struct
+// useful for when all handlers are in the same package
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
+	// Define a new command-line flag with the name 'addr', a default value of ":4000"
+	// and some short help text explaining what the flag controls
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Importantly, we use the flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors are
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
 
-	// create file server
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil)) // initialize a new structured logger
 
-	// register the file server as the handler for all URL paths
-	// that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	app := &application{
+		logger: logger,
+	} // initialize a new instance of applicaiton struct containing dependencies
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	// Use the Info() method to log the starting server message at Info severity
+	// (along with the listen address as an attribute).
+	logger.Info("starting server", slog.String("addr", *addr))
 
-	log.Print("starting server on :4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// And we pass the dereferenced addr pointer to http.ListenAndServe() too.
+	mux := app.routes()
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+// The routes() method returns a servemux containing our application routes.
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	// create file server
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// register the file server as the handler for all URL paths
+	// that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
+	return mux
+}
